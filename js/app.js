832e817464d1f361c970d0ad95e87f609d55191f
monavaroqua <monavaroqua@gmail.com>
2021-02-16 10:48:49 +0200

    guess game

--- js/app.js
+++ js/app.js
@@ -4,4 +4,30 @@
        alert (' welcome ' + userName + ' kindly answer these questions');
        console.log(userName);
 
+    
+       let firstQuestion = prompt('do i have a pet?');
+       while (firstQuestion !== 'yes' && firstQuestion !== 'no') {
+           firstQuestion = prompt ('please answer with yes or no');
+       }
+
        
+       
+    
+       let secondQuestion = prompt('do i live in jordan');
+       while (secondQuestion !== 'yes' && secondQuestion !== 'no') {
+           secondQuestion = prompt ('please answer with yes or no');
+       }
+
+       
+       let thirdQuestion = prompt('do i love sweets?');
+       while (thirdQuestion !== 'yes' && thirdQuestion !== 'no') {
+           thirdQuestion = prompt ('please answer with yes or no');
+       }
+
+
+       let fourthQuestion = prompt('am i a gamer?');
+       while (forthQuestion !== 'yes' && forthQuestion !== 'no') {
+           forthQuestion = prompt ('please answer with yes or no');
+       }
+
+      
